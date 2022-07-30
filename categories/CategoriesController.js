a0a0ed445257ba202e69a8af31a117956a5d5cb8
Ilailson <ilailson@hotmail.com>
2022-07-29 22:58:24 -0300

    Definindo rotas formulario e salvar no banco

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,15 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const Category = require("./Category") //salvar - banco
+const slugify = require('slugify')
 
-router.get('/categories', (req, res) => {
-    res.send('Rota de categorias')
-}
-)
-
-router.get('/admin/categories/new', (req, res) => {
-    res.send("Rota para nova categoria")
-
+router.get("/admin/categories/new",(req, res) => {
+    res.render("admin/categories/new")
 })
 
+
+//slug remove espaço
+router.post("/categories/save",(req, res) => {
+    var title = req.body.title //pegando - formulario
+    if(title != undefined){
+        
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {res.redirect("/")})
+
+    }else{
+        res.redirect("/admin/categories/new")
+    }
+
+})
 
 module.exports = router
