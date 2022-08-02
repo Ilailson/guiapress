38b750c7b3797c065f2e5162e0993dc9094c3b5b
Ilailson <ilailson@hotmail.com>
2022-08-02 20:50:36 -0300

    Rota - salvar - banco

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
+
+//savar - banco
 const Category = require('../categories/Category')
+const slugify = require('slugify')
+const Article = require('./Article')
+
 
 router.get('/articles', (req, res) => {
     res.send('Rota de artigons')
@@ -22,5 +27,19 @@ router.get('/admin/articles/new', (req, res) => {
 })
 
 
+router.post('/articles/save', (req, res) => {
+    var title = req.body.title
+    var body = req.body.body
+    var categoryId = req.body.category
+
+    //salvando - artigo
+
+    Article.create({
+        title:title, 
+        slug: slugify(title),
+        body:body,
+        categoryId: categoryId
+    }).then(() => res.redirect('/articles/'))
+})
 
 module.exports = router
