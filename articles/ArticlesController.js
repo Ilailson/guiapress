62ce931e3711c154eeddd1cef7f1d7ab071cc118
Ilailson <ilailson@hotmail.com>
2022-08-14 16:20:51 -0300

    Rota pegar id - mostrar informações - para edição

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -64,22 +64,27 @@ router.post("/articles/delete",(req, res) =>{
     }
 })
 
-router.get("/admin/articles/edit/:id",(req, res) =>{
-    var id = req.params.id 
+
+/*
+rota para pegar o id passado por parametro e mostrar na view
+*/
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id
 
     Article.findByPk(id).then(article => {
-        if(article !== undefined) {
+        if(article != undefined) {
+            //passando a listagem das categorias. 
             Category.findAll().then(categories => {
-
-                res.render("admin/articles/edit",{categories: categories}) //para exibir o laço na pagina edit
+                res.render("admin/articles/edit", {article: article, categories: categories})
             })
-
         }else {
             res.redirect("/")
         }
-    }).catch(error => {
-        res.redirect("/")
-    })
     
+    })
+.catch(err => {
+    res.redirect("/")
 })
+})
+
 module.exports = router
